4c6aed3090aed7f104b82230ae5c683a00ef4ef8
Mazin <code.mazin.m@gmail.com>
2025-03-09 23:07:46 +1100

    done

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,3 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director, :summary
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director
 end
